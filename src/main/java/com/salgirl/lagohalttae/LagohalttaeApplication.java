e50603127d6a8e0e14bab4e11b626b89eaf02cee
SeokyungKim <kskyung0624@gmail.com>
2022-05-30 00:00:16 -0700

    Env : Dev 환경 세팅

---
+++ src/main/java/com/salgirl/lagohalttae/LagohalttaeApplication.java
@@ -0,0 +1,13 @@
+package com.salgirl.lagohalttae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagohalttaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagohalttaeApplication.class, args);
+	}
+
+}
